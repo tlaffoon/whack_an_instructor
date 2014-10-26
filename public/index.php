5cef95c0ef23827520b7c8b6f49b651f9e0aa542
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-10-25 21:58:48 -0500

    Initial commit.

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title>Whack A Coder</title>
+</head>
+<body>
+
+<div class="main-box">
+    <div class="lil-box" id="1"></div>
+    <div class="lil-box" id="2"></div>
+    <div class="lil-box" id="3"></div>
+    <div class="lil-box" id="4"></div>
+    <div class="lil-box" id="5"></div>
+    <div class="lil-box" id="6"></div>
+    <div class="lil-box" id="7"></div>
+    <div class="lil-box" id="8"></div>
+    <div class="lil-box" id="9"></div>
+</div>
+
+</body>
+<script type="text/javascript">
+    // fade in each random box
+    // add event listener on faded in box
+    // when clicked:
+        // add points
+        // decrease interval between fade ins
+        // make a sound
+        // remove listener on said box when clicked
+</script>
+</html>
